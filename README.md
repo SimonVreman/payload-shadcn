458b09fcdececfb28a3eb00ef18579f7c28e1888
Simon Vreman <info@simonvreman.nl>
2024-04-13 11:02:53 +0200

    init example

---
+++ README.md
@@ -0,0 +1,10 @@
+Steps to create this:
+
+- `pnpx create-next-app`
+- Create a new directory in ./src/app such as ./src/app/(app)
+- Move all files from ./src/app into that directory
+- `pnpx create-payload-app@beta`
+- `pnpx shadcn-ui@latest init`
+- setup font (as per shadcn guide https://ui.shadcn.com/docs/installation/next)
+- `pnpx shadcn-ui@latest add button`
+- Done!
---
+++ src/payload.config.ts
@@ -0,0 +1,36 @@
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+// import { payloadCloud } from '@payloadcms/plugin-cloud'
+import { lexicalEditor } from '@payloadcms/richtext-lexical' // editor-import
+import path from 'path'
+import { buildConfig } from 'payload/config'
+// import sharp from 'sharp'
+import { fileURLToPath } from 'url'
+
+import { Users } from './collections/Users'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users],
+  editor: lexicalEditor({}),
+  // plugins: [payloadCloud()], // TODO: Re-enable when cloud supports 3.0
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+  }),
+  // Sharp is now an optional dependency -
+  // if you want to resize images, crop, set focal point, etc.
+  // make sure to install it and pass it to the config.
+
+  // This is temporary - we may make an adapter pattern
+  // for this before reaching 3.0 stable
+
+  // sharp,
+})
